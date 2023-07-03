d26b084300d5f0227a6c7df22e4c564d5bc254a7
Niko <nikivyro@gmail.com>
2023-07-03 16:34:36 +0200

    Last update D4

--- D4/js/main.js
+++ D4/js/main.js
@@ -1,13 +1,15 @@
 window.onload = (event) => {
     // Funzione che conta tutte le card della pagina
     let allCards = document.querySelectorAll(".card");
-    console.log(allCards.length);
+    // console.log(allCards.length);
     alert(`Nella pagina sono presenti ${allCards.length} viaggi`);
-    
-    // Funzione per rimuovere le card della pagina
-    function removeCard (){
-        for (const card of allCards) {
-            card.remove()
-        }
+};
+
+// Funzione per rimuovere le card della pagina
+const removeCards = function () {
+    let allCards = document.querySelectorAll(".card");
+    for (let i = 0; i < allCards.length; i++) {
+        const card = allCards[i];
+        card.remove();
     }
 };
